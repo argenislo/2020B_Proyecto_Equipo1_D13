6937a83acf3cb6bc4f5ca84a226d0b18caf8c91f
Argenis Isaí López Robles <75322836+argenislo@users.noreply.github.com>
2020-12-02 14:46:53 -0600

    Add files via upload

---
+++ Mux2_1_32.v
@@ -0,0 +1,31 @@
+//1 - Declaracion del modulo
+module Mux2_1_32(
+	input [31:0]inRes, inMem,
+	input mMemSel,
+	output reg [31:0]mWrite
+);
+
+//2 - Declaracion de cables y registros
+
+//3 - Cuerpo del modulo
+
+//Bloques secuenciales
+always @*
+begin
+	case(mMemSel)
+	1'b0:
+		begin
+			mWrite = inRes;
+		end
+	1'b1:
+		begin
+			mWrite = inMem;
+		end
+	default:
+		begin
+			mWrite = 32'bz;
+		end
+	endcase
+end
+
+endmodule

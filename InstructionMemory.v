19cc387d97decff096dd9e6eeb4583faa6f775c3
Argenis Isaí López Robles <75322836+argenislo@users.noreply.github.com>
2020-12-04 12:18:50 -0600

    Update InstructionMemory.v

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -12,20 +12,44 @@ reg [31:0]inMem [0:127];
 //Bloque secuencial
 always @(readAddress)
 begin
-	Instruction = inMem[readAddress];
+	Instruction = {inMem[readAddress], inMem[readAddress + 1], inMem[readAddress + 2], inMem[readAddress + 3]};
 end
 
 //Inicializacion
 initial
 begin
-	inMem[0] = 32'b000000_01110_01010_10110_00000_100000;	//ADD
-	inMem[1] = 32'b000000_01110_01010_10110_00000_100010;	//SUB
-	inMem[2] = 32'b011100_01110_01010_10110_00000_000010;	//MUL
-	inMem[3] = 32'b000000_01110_01010_10110_00000_011010;	//DIV
-	inMem[4] = 32'b000000_01110_01010_10110_00000_100101;	//OR
-	inMem[5] = 32'b000000_01110_01010_10110_00000_100100;	//AND
-	inMem[6] = 32'b000000_01110_01010_10110_00000_101010;	//SLT
-	inMem[7] = 32'b000000_01110_01010_10110_00000_000000;	//NOP
+	inMem[0] = 8'b00000001;		//ADD
+	inMem[1] = 8'b11001010;
+	inMem[2] = 8'b10110000;
+	inMem[3] = 8'b00100000;
+	inMem[4] = 8'b00000001;		//SUB
+	inMem[5] = 8'b11001010;
+	inMem[6] = 8'b10110000;
+	inMem[7] = 8'b00100010;
+	inMem[8] = 8'b01110001;		//MUL
+	inMem[9] = 8'b11001010;
+	inMem[10] = 8'b10110000;
+	inMem[11] = 8'b00000010;
+	inMem[12] = 8'b00000001;	//DIV
+	inMem[13] = 8'b11001010;
+	inMem[14] = 8'b10110000;
+	inMem[15] = 8'b00011010;
+	inMem[16] = 8'b00000001;	//OR
+	inMem[17] = 8'b11001010;
+	inMem[18] = 8'b10110000;
+	inMem[19] = 8'b00100101;
+	inMem[20] = 8'b00000001;	//AND
+	inMem[21] = 8'b11001010;
+	inMem[22] = 8'b10110000;
+	inMem[23] = 8'b00100100;
+	inMem[24] = 8'b00000001;	//SLT
+	inMem[25] = 8'b11001010;
+	inMem[26] = 8'b10110000;
+	inMem[27] = 8'b00101010;
+	inMem[28] = 8'b00000001;	//NOP
+	inMem[29] = 8'b11001010;
+	inMem[30] = 8'b10110000;
+	inMem[31] = 8'b00000000;
 end
 
 endmodule
